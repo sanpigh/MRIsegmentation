aa3385a143e0d696493f07c1f6ddc1315b3cb085
Guillaume Alleon <guillaume.alleon@gmail.com>
2021-06-30 09:48:00 +0200

    Simplify read dataset

--- MRIsegmentation/data.py
+++ MRIsegmentation/data.py
@@ -7,18 +7,9 @@ import os
 import random
 
 from google.cloud import storage
-from http.client import LENGTH_REQUIRED
 import tensorflow as tf
 
 from MRIsegmentation.params import GDRIVE_DATA_PATH, BUCKET_NAME, BUCKET_DATA_PATH
-
-
-def pos_neg_diagnosis(mask_path):
-    value = np.max(cv2.imread(mask_path))
-    if value > 0:
-        return 1
-    else:
-        return 0
 
 
 def get_data(nrows=1_000):
@@ -35,50 +26,7 @@ def get_data(nrows=1_000):
 
 
 def get_data_from_drive():
-    data = pd.read_csv(GDRIVE_DATA_PATH + "/kaggle_3m/data.csv")
-
-    data_map = []
-
-    for sub_dir_path in glob.glob(GDRIVE_DATA_PATH + "kaggle_3m/*"):
-        # if os.path.isdir(sub_path_dir):
-        try:
-            dir_name = sub_dir_path.split("/")[-1]
-            for filename in os.listdir(sub_dir_path):
-                image_path = sub_dir_path + "/" + filename
-                data_map.extend([dir_name, image_path])
-        except Exception as e:
-            logging.info(e)
-
-    df = pd.DataFrame({"patient_id": data_map[::2], "path": data_map[1::2]})
-
-    df_imgs = df[~df["path"].str.contains("mask")]  # if have not mask
-
-    df_masks = df[df["path"].str.contains("mask")]  # if have mask
-
-    # File path line length images for later sorting
-    BASE_LEN = len(
-        "/content/drive/MyDrive/MRIsegmentation/kaggle_3m/TCGA_DU_6404_19850629/TCGA_DU_6404_19850629_"
-    )  # <-!!!43.tif)
-    END_IMG_LEN = 4
-    END_MASK_LEN = 9
-
-    # Data sorting
-    imgs = sorted(df_imgs["path"].values, key=lambda x: int(x[BASE_LEN:-END_IMG_LEN]))
-    masks = sorted(
-        df_masks["path"].values, key=lambda x: int(x[BASE_LEN:-END_MASK_LEN])
-    )
-
-    brain_df = pd.DataFrame(
-        {
-            "patient_id": df_imgs.patient_id.values,
-            "image_path": imgs,
-            "mask_path": masks,
-        }
-    )
-
-    brain_df["mask"] = brain_df["mask_path"].apply(lambda x: pos_neg_diagnosis(x))
-
-    return brain_df
+    return pd.read_csv(GDRIVE_DATA_PATH + "/kaggle_3m/brain_df.csv")
 
 
 def list_blobs(bucket_name):
@@ -99,7 +47,7 @@ def list_blobs(bucket_name):
 def holdout(df, train_ratio=0.8):
 
     img_paths = df["image_path"].values
-    msk_paths = df["mmask_path"].values
+    msk_paths = df["mask_path"].values
 
     full_size = df.shape[0]
 
