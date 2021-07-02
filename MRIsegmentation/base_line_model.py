795c96d67174545d3c656a46b21e4939a2e48133
Santiago Pighin <pighin.santiago@gmail.com>
2021-07-02 10:25:57 +0200

    Adding __init__ to BaseLineModel

--- MRIsegmentation/base_line_model.py
+++ MRIsegmentation/base_line_model.py
@@ -10,31 +10,33 @@ from os import walk # to navigate along directories
 
 
 class BaseLineModel():
+    def __init__(self, red_threshold_coef, green_threshold_coef, blue_threshold_coef):
+        self.red_threshold_coef   = red_threshold_coef
+        self.green_threshold_coef = green_threshold_coef
+        self.blue_threshold_coef  = blue_threshold_coef
 
     def summary(self):
       return f'Baseline model'
   
-    def score(self, X, y_true, red_threshold_coef=1.5, green_threshold_coef=1.7, blue_threshold_coef=1.5):
-        y_pred = self.predict(X, red_threshold_coef, green_threshold_coef, blue_threshold_coef)
-#        IoU = IoU_score(y_true, y_pred)
+    def score(self, X, y_true):
+        y_pred = self.predict(X)
         IoU = tversky(tf.cast(y_true, dtype=tf.float32), tf.cast(y_pred, dtype=tf.float32))
         return IoU
 
-    def predict(self, X, red_threshold_coef=1.5, green_threshold_coef=1.7, blue_threshold_coef=1.5):
+    def predict(self, X):
     # Segmentate tumor based on color
     # Input: tensor (number_of_images, image_height, image_width, channel). 
     # Output: calculated mask (number_of_images, image_height, image_width, channel)
  
         mask = None
-        print(f'Number of images already processed:')
-
+        print(f'Processing all the images:\n')
         for index, img in enumerate(X):
             if index%30 == 0:
                 print('\n')
-            print(f'{index+1}  ', end='')
-            red_threshold   = tf.math.reduce_mean(img[:, :, 0]).numpy() + red_threshold_coef   * tf.math.reduce_std(img[:, :, 0].numpy())
-            green_threshold = tf.math.reduce_mean(img[:, :, 1]).numpy() + green_threshold_coef * tf.math.reduce_std(img[:, :, 1].numpy())
-            blue_threshold  = tf.math.reduce_mean(img[:, :, 2]).numpy() + blue_threshold_coef  * tf.math.reduce_std(img[:, :, 2].numpy())
+            print('.', end='')
+            red_threshold   = tf.math.reduce_mean(img[:, :, 0]).numpy() + self.red_threshold_coef   * tf.math.reduce_std(img[:, :, 0].numpy())
+            green_threshold = tf.math.reduce_mean(img[:, :, 1]).numpy() + self.green_threshold_coef * tf.math.reduce_std(img[:, :, 1].numpy())
+            blue_threshold  = tf.math.reduce_mean(img[:, :, 2]).numpy() + self.blue_threshold_coef  * tf.math.reduce_std(img[:, :, 2].numpy())
 
             red_pass   = tf.cast(img[:,:,0] < red_threshold, tf.int32)
             green_pass = tf.cast(img[:,:,1] > green_threshold, tf.int32)
