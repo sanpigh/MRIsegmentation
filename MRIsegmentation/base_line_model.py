badf4ccc0cada8188e52359cb27ecac957d4c63b
Santiago Pighin <pighin.santiago@gmail.com>
2021-07-02 14:18:22 +0200

    last version minor modifications

--- MRIsegmentation/base_line_model.py
+++ MRIsegmentation/base_line_model.py
@@ -1,16 +1,14 @@
-from google.colab import drive
 import pandas as pd
 import matplotlib.pyplot as plt
 
 from MRIsegmentation.data import get_data_from_drive
 from MRIsegmentation.utils import tversky
 import tensorflow as tf
-from os import walk # to navigate along directories
 
 
 
 class BaseLineModel():
-    def __init__(self, red_threshold_coef, green_threshold_coef, blue_threshold_coef):
+    def __init__(self, red_threshold_coef=1.5, green_threshold_coef=1.7, blue_threshold_coef=1.5):
         self.red_threshold_coef   = red_threshold_coef
         self.green_threshold_coef = green_threshold_coef
         self.blue_threshold_coef  = blue_threshold_coef
@@ -29,11 +27,10 @@ class BaseLineModel():
     # Output: calculated mask (number_of_images, image_height, image_width, channel)
  
         mask = None
-        print(f'Processing all the images:\n')
+        print(f'Number of images processed:')
         for index, img in enumerate(X):
-            if index%30 == 0:
-                print('\n')
-            print('.', end='')
+            if index%1000 == 0:
+                print(f'{index} ', end='')
             red_threshold   = tf.math.reduce_mean(img[:, :, 0]).numpy() + self.red_threshold_coef   * tf.math.reduce_std(img[:, :, 0].numpy())
             green_threshold = tf.math.reduce_mean(img[:, :, 1]).numpy() + self.green_threshold_coef * tf.math.reduce_std(img[:, :, 1].numpy())
             blue_threshold  = tf.math.reduce_mean(img[:, :, 2]).numpy() + self.blue_threshold_coef  * tf.math.reduce_std(img[:, :, 2].numpy())
@@ -56,47 +53,17 @@ class BaseLineModel():
 
 
 def base_line_model_retrieve():
-    drive.mount('/content/drive')
-    
-    # get data
-    GDRIVE_DATA_PATH = '/content/drive/MyDrive/MRIsegmentation/'
-    path = GDRIVE_DATA_PATH + 'kaggle_3m/'
-#    path = 'raw_data/kaggle_3m/'    # to run local
-    df = pd.read_csv(path+'data.csv')
-
-
-    #### MY DICTIONARY WITH ALL THE IMAGES
-    file_dic = {}
-    for root, subdirs, files in walk(path):
-        if root != path: # Eliminate the list of directories at the begining of the walk
-            file_dic[root] = [file for file in files if 'mask' not in file]
-
-
     # RETRIEVE DATA
-
+    df = get_data_from_drive()
     # Create X_train and y_train from images and masks
     X_train   = []
     y_train   = []
-    file_name = []
 
-    for folder, images in file_dic.items():
-        print(folder)
-        for image in images:
-            mask = image[:-4] + '_mask.tif'
-            X_train.append(plt.imread(folder+'/'+image))
-            y_train.append(plt.imread(folder+'/'+mask))
-
+    for index, row in df.iloc[0:100].iterrows():
+        X.append(plt.imread(row['image_path']))
+        y.append(plt.imread(row['mask_path']))
 
     X_train_tensor = tf.convert_to_tensor(X_train, dtype=tf.float32)
     y_train_tensor = tf.convert_to_tensor(y_train, dtype=tf.float32)
 
-    print(tf.shape(X_train_tensor))
-    print(tf.shape(y_train_tensor))
     return X_train_tensor, y_train_tensor
-
-
-def base_line_model_run(X_train_tensor, y_train_tensor):
-    base = BaseLineModel()
-    print('Calculating the baseline')
-    score = base.score(X_train_tensor, y_train_tensor).numpy()
-    return score
