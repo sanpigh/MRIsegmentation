e97b99530efabf2a180813e5cf1812ef404a764c
Guillaume Alleon <guillaume.alleon@gmail.com>
2021-07-03 08:22:38 +0200

    Adapting contrast value to PR version

--- MRIsegmentation/utils.py
+++ MRIsegmentation/utils.py
@@ -114,13 +114,14 @@ def augment_data(image, mask):
     """
     choice = tf.random.uniform((), minval=0, maxval=1)
     if True and choice < 0.5:
-        delta_val = tf.random.uniform((), minval=-0.5, maxval=0.5)
-        image = tf.image.adjust_brightness(image, delta=delta_val)
+        brightness_val = tf.random.uniform((), minval=-0.3, maxval=0.3)
+        image = tf.image.adjust_brightness(image, delta=brightness_val)
         return image, mask
 
     choice = tf.random.uniform((), minval=0, maxval=1)
     if True and choice < 0.5:
-        image = tf.image.adjust_contrast(image, contrast_factor=0.1)
+        contrast_val = tf.random.uniform((), minval=0.40, maxval=0.45)
+        image = tf.image.adjust_contrast(image, contrast_factor=contrast_val)
         return image, mask
 
     choice = tf.random.uniform((), minval=0, maxval=1)
